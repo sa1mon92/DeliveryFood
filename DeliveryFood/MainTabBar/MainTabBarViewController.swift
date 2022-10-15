44e2912acd4c10233213457542b2c14ec3f3025e
Dmitrii Sadyrev <79851806+sa1mon92@users.noreply.github.com>
2022-10-15 22:08:47 +0300

    add images

--- DeliveryFood/MainTabBar/MainTabBarViewController.swift
+++ DeliveryFood/MainTabBar/MainTabBarViewController.swift
@@ -13,5 +13,9 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         view.tintColor = Constants.categoryColor
         tabBar.backgroundColor = .white
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
+        tabBar.layer.shadowRadius = 1
+        tabBar.layer.shadowColor = UIColor.black.cgColor
+        tabBar.layer.shadowOpacity = 0.5
     }
 }
